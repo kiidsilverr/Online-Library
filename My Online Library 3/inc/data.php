b5a809f5a32e95f64ace3bee6db5e3682390bade
kiidsilverr <50423246+kiidsilverr@users.noreply.github.com>
2019-06-03 18:13:54 -0400

    Add files via upload

---
+++ My Online Library 3/inc/data.php
@@ -0,0 +1,178 @@
+<?php
+$catalog=[];
+//Books
+$catalog[101] = [
+ "title" => "If you Give a Mouse a Cookie",
+ "img" => "img/media/cookie.jpg",
+ "genre" => "Childrens Literature",
+ "format" => "Paperback",
+ "year" => 2000,
+ "category" => "Books",
+ "authors" => [
+     "Felcia Bond",
+ ],
+ "publisher" => "Laura Numeroff",
+ "isbn" => '9780064434119'
+];
+
+$catalog[102] = [
+ "title" => "Chronicles of Narnia",
+ "img" => "img/media/narnia.jpg",
+ "genre" => "Fantasy-Magic Books",
+ "format" => "Paperback",
+ "year" => 2010,
+ "category" => "Books",
+ "authors" => [
+     "C. S. Lewis",
+ ],
+ "publisher" => "HarperCollins Narnia; Media Tie In edition",
+ "isbn" => '978-0061992889'
+];
+
+$catalog[103] = [
+ "title" => "The Magicians",
+ "img" => "img/media/magicians.jpg",
+ "genre" => "Fiction",
+ "format" => "Paperback",
+ "year" => 2009,
+ "category" => "Books",
+ "authors" => [
+     "Lev Grossman",
+ ],
+ "publisher" => "	Viking Penguin Books",
+ "isbn" => '9781101081440'
+];
+
+$catalog[104] = [
+ "title" => "Lord of The Flies",
+ "img" => "img/media/LOTF.jpg",
+ "genre" => "Fantasy",
+ "format" => "Paperback",
+ "year" => 1954,
+ "category" => "Books",
+ "authors" => [
+     "William Golding",
+ ],
+ "publisher" => "Faber and Faber",
+ "isbn" => '0-571-05686-5'
+];
+//Movies
+$catalog[201] = [
+ "title" => "Shrek",
+ "img" => "img/media/Shrek.jpg",
+ "genre" => "Action,Adventure,Fantasy",
+ "format" => "DVD",
+ "year" => 2001,
+ "category" => "Movies",
+ "director" => "Andrew Adamson",
+ "writers" => [
+     "Ted Elliott,
+Terry Rossio,
+Joe Stillman",
+ ],
+ "stars" => [
+     " Mike Myers,
+     Eddie Murphy,
+     Cameron Diaz,
+     and John Lithgow"
+ ]
+];
+
+$catalog[202] = [
+ "title" => "Avengers Endgame",
+ "img" => "img/media/endgame.jpg",
+ "genre" => "Action,Adventure,Fantasy",
+ "format" => "DVD",
+ "year" => 2019,
+ "category" => "Movies",
+ "director" => ["Joe Russo","Anthony Russo"],
+ "writers" => [
+   "Christopher Markus",
+ "Stephen McFeely"
+ ],
+ "stars" => [
+   "Robert Downey Jr.",
+   "Chris Hemsworth",
+   "Scarlett Johansson"
+ ]
+];
+
+$catalog[203] = [
+ "title" => "AVENGERS: INFINITY WAR",
+ "img" => "img/media/avengers.jpg",
+ "genre" => "Action,Adventure,Fantasy",
+ "format" => "DVD",
+ "year" => 2018,
+ "category" => "Movies",
+ "director" => ["Joe Russo","Anthony Russo"],
+ "writers" => [
+     "Christopher Markus",
+	 "Stephen McFeely"
+ ],
+ "stars" => [
+     "Robert Downey Jr.",
+     "Chris Hemsworth",
+     "Scarlett Johansson"
+ ]
+];
+
+$catalog[204] = [
+ "title" => "What Happened to Monday",
+ "img" => "img/media/monday.jpg",
+ "genre" => "Action and Adventure",
+ "format" => "DVD",
+ "year" => 2017,
+ "category" => "Movies",
+ "director" => "Tommy Wirkola",
+ "writers" => [
+     "	Max Botkin,
+Kerry Williamson"
+ ],
+ "stars" => [
+     "Noomi Rapace",
+     "Willem Dafoe",
+     "Glenn Close"
+ ]
+];
+
+//Music
+$catalog[301] = [
+ "title" => "Cheng Yeng",
+ "img" => "img/media/shensea.jpg",
+ "genre" => "Dancehall",
+ "format" => "CD",
+ "year" => 2016,
+ "category" => "Music",
+ "artist" => "Chensea Lee"
+];
+
+$catalog[302] = [
+ "title" => "The Migos",
+ "img" => "img/media/migos.jpg",
+ "genre" => "Rap",
+ "format" => "CD",
+ "year" => 2008,
+ "category" => "Music",
+ "artist" => "The Migos"
+];
+
+$catalog[303] = [
+ "title" => "Beer Bongs and Bentleys",
+ "img" => "img/media/posty.jpg",
+ "genre" => "Pop",
+ "format" => "CD",
+ "year" => 2014,
+ "category" => "Music",
+ "artist" => "Post Malone"
+];
+
+$catalog[304] = [
+ "title" => "Aura",
+ "img" => "img/media/ozuna3.jpg",
+ "genre" => "Latino Rap",
+ "format" => "Album",
+ "year" => 2018,
+ "category" => "Music",
+ "artist" => "Ozuna Rosado"
+];
+?>
